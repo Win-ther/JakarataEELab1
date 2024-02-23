c12470e7223bd4c15e966be5cc847049b8b885f3
altmannk <katherine.altmann@iths.se>
2024-02-23 15:25:38 +0100

    Add CRUD functionality
    Co-authored-by: Katherine Altmann <katherine.altmann@iths.se>
    Co-authored-by: Emil Winther <emil.winther@hotmail.com>

--- src/main/java/se/iths/jakartaeelab1/repository/PersonRepository.java
+++ src/main/java/se/iths/jakartaeelab1/repository/PersonRepository.java
@@ -3,6 +3,10 @@ package se.iths.jakartaeelab1.repository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
+import se.iths.jakartaeelab1.entity.Person;
 
 @ApplicationScoped
 public class PersonRepository {
@@ -10,6 +14,24 @@ public class PersonRepository {
     @PersistenceContext(unitName = "mysql")
     EntityManager entityManager;
 
+    @Transactional
+    public Person createOrUpdate(Person person) {
+        entityManager.persist(person);
+        return person;
+    }
 
+    public List<Person> readAll() {
+        return entityManager
+                .createQuery("select p from Person p", Person.class)
+                .getResultList();
+    }
+
+    public Person findById(UUID id) {
+        return entityManager.find(Person.class, id);
+    }
+
+    public void deletePerson(Person person) {
+        entityManager.remove(person);
+    }
 
 }
