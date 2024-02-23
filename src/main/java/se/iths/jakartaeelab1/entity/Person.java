9a1bff6359a0202919a0c22f8a91cd05cb4cd1e2
Win-ther <emil.winther@hotmail.com>
2024-02-23 11:14:55 +0100

    Add entity Person and table will be created on start of server.

---
+++ src/main/java/se/iths/jakartaeelab1/entity/Person.java
@@ -0,0 +1,61 @@
+package se.iths.jakartaeelab1.entity;
+
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String profession;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return getId() != null && Objects.equals(getId(), person.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
+    }
+}
--- src/main/java/se/iths/jakartaeelab1/resource/HelloResource.java
+++ src/main/java/se/iths/jakartaeelab1/resource/HelloResource.java
@@ -1,4 +1,4 @@
-package se.iths.jakartaeelab1;
+package se.iths.jakartaeelab1.resource;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
