169e71fe1820c628f2858ec404d2b1101b203efd
Win-ther <emil.winther@hotmail.com>
2024-02-27 14:41:12 +0100

    Add allPersons-method

--- src/main/java/se/iths/jakartaeelab1/service/PersonService.java
+++ src/main/java/se/iths/jakartaeelab1/service/PersonService.java
@@ -2,10 +2,13 @@ package se.iths.jakartaeelab1.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import se.iths.jakartaeelab1.dto.PersonDto;
+import se.iths.jakartaeelab1.dto.Persons;
 import se.iths.jakartaeelab1.entity.Person;
 import se.iths.jakartaeelab1.repository.PersonRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 @ApplicationScoped
 public class PersonService {
@@ -16,4 +19,7 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
+    public Persons allPersons(){
+        return new Persons(personRepository.readAllPerson().stream().map(PersonDto::map).toList());
+    }
 }
