f548be3240d3a2661391b411e6f7256631d531de
altmannk <katherine.altmann@iths.se>
2024-02-27 14:21:12 +0100

    Add map to return PersonDto

--- src/main/java/se/iths/jakartaeelab1/dto/PersonDto.java
+++ src/main/java/se/iths/jakartaeelab1/dto/PersonDto.java
@@ -2,7 +2,12 @@ package se.iths.jakartaeelab1.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
+import se.iths.jakartaeelab1.entity.Person;
 
 public record PersonDto(@NotEmpty String name, @Positive int age, @NotEmpty String profession) {
+
+    public static PersonDto map(Person person) {
+        return new PersonDto(person.getName(), person.getAge(), person.getProfession());
+    }
 
 }
