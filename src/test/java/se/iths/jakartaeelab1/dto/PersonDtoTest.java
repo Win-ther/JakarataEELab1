cbeb244e5bc2138b402eb66c2cefae743afb3460
charlotte henriksson <chaallee_@hotmail.com>
2024-03-01 16:24:55 +0100

    added validators and hibernate.validator in pom.xml, working on test for personTest

--- src/test/java/se/iths/jakartaeelab1/dto/PersonDtoTest.java
+++ src/test/java/se/iths/jakartaeelab1/dto/PersonDtoTest.java
@@ -1,13 +1,30 @@
 package se.iths.jakartaeelab1.dto;
 
+import jakarta.validation.*;
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import se.iths.jakartaeelab1.dto.PersonDto;
 import se.iths.jakartaeelab1.entity.Person;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
+
+
 public class PersonDtoTest {
+    private static Validator validator;
+
+    @BeforeAll
+    static void setupValidator() {
+        ValidatorFactory factory = Validation.byDefaultProvider()
+                .configure()
+                .messageInterpolator(new ParameterMessageInterpolator())
+                .buildValidatorFactory();
+        validator = factory.getValidator();
+    }
+
     @Test
     void map_shouldMapPersonToPersonDto() {
         Person person = new Person();
@@ -17,19 +34,43 @@ public class PersonDtoTest {
 
         PersonDto personDto = PersonDto.map(person);
 
-        assertEquals("John", person.getName());
-        assertEquals(25, person.getAge());
-        assertEquals("Developer", person.getProfession());
+        assertEquals("John", personDto.name());
+        assertEquals(25, personDto.age());
+        assertEquals("Developer", personDto.profession());
+
+        PersonDto mappedPersonDto = PersonDto.map(person);
+
+        Set<ConstraintViolation<PersonDto>> violations = validator.validate(mappedPersonDto);
+
+        assertTrue(violations.isEmpty(), "Validation should pass for mapping Person to PersonDto");
     }
 
     @Test
     void map_shouldMapPersonDtoToPerson() {
-        PersonDto personDto = new PersonDto("Jane", 30, "Engineer");
+        PersonDto personDto = new PersonDto(
+                "Jane",
+                30,
+                "Engineer");
 
         Person person = PersonDto.map(personDto);
 
         assertEquals("Jane", person.getName());
         assertEquals(30, person.getAge());
         assertEquals("Engineer", person.getProfession());
+
+        PersonDto personDtoInput = new PersonDto("Jane", 30, "Engineer");
+        Set<ConstraintViolation<PersonDto>> violations = validator.validate(personDtoInput);
+
+        assertTrue(violations.isEmpty(), "Validation should pass for mapping PersonDto to Person");
+
+        Person mappedPerson = PersonDto.map(personDtoInput);
+
+        assertEquals("Jane", mappedPerson.getName());
+        assertEquals(30, mappedPerson.getAge());
+        assertEquals("Engineer", mappedPerson.getProfession());
     }
+
+
 }
+
+
