646080bf7f6b0959a4ca7960bfe0758ac254857a
altmannk <katherine.altmann@iths.se>
2024-03-01 12:37:13 +0100

    Add GET to retrieve person by Id, Add POST to add person

--- src/main/java/se/iths/jakartaeelab1/resource/PersonResource.java
+++ src/main/java/se/iths/jakartaeelab1/resource/PersonResource.java
@@ -1,10 +1,16 @@
 package se.iths.jakartaeelab1.resource;
 
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import se.iths.jakartaeelab1.dto.PersonDto;
 import se.iths.jakartaeelab1.dto.Persons;
 import se.iths.jakartaeelab1.service.PersonService;
+
+import java.net.URI;
+import java.util.UUID;
 
 @Path("/persons")
 public class PersonResource {
@@ -23,6 +29,20 @@ public class PersonResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Persons getAllPersons() {
         return personService.allPersons();
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/{id}")
+    public PersonDto getPersonById(@PathParam("id") UUID id) {
+        return personService.onePerson(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addPerson(@Valid PersonDto personDto) {
+        var person = personService.addPerson(personDto);
+        return Response.created(URI.create("http://localhost:8080/labb1/api/persons/" + person.getId())).build();
     }
 
 }
