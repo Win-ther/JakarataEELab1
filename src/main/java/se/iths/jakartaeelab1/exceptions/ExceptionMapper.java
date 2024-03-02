2f1b9ea5b09539b0f6242aeb240d4d63736c1c39
Daniel Bäckberg <143079386+Danback@users.noreply.github.com>
2024-03-02 12:30:24 +0100

    Update src/main/java/se/iths/jakartaeelab1/exceptions/ExceptionMapper.java
    Co-authored-by: Emil Winther <143023413+Win-ther@users.noreply.github.com>

--- src/main/java/se/iths/jakartaeelab1/exceptions/ExceptionMapper.java
+++ src/main/java/se/iths/jakartaeelab1/exceptions/ExceptionMapper.java
@@ -1,43 +1,45 @@
 package se.iths.jakartaeelab1.exceptions;
 
-import jakarta.ws.rs.core.MediaType;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
+import java.util.Set;
+
 @Provider
-public class ExceptionMapper implements jakarta.ws.rs.ext.ExceptionMapper<Exception> {
-
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+    @Context
+    UriInfo uriInfo;
     @Override
-    public Response toResponse(Exception exception) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setError(exception.getMessage());
-        errorResponse.setCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
+    public Response toResponse(ConstraintViolationException exception) {
+        Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
 
-        return Response
-                .status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(errorResponse)
-                .type(MediaType.APPLICATION_JSON)
-                .build();
-    }
+        final var jsonObjectBuilder = Json
+                .createObjectBuilder()
+                .add("host",uriInfo.getAbsolutePath().getHost())
+                .add("resource",uriInfo.getAbsolutePath().getPath())
+                .add("title","Validation Errors");
 
-    public static class ErrorResponse {
-        private int code;
-        private String error;
+        final var jsonArray = Json.createArrayBuilder();
 
-        public int getCode() {
-            return code;
+        for (final var constraint : constraintViolations) {
+            String message = constraint.getMessage();
+            String violatedField = constraint.getPropertyPath().toString().split("\\.")[2];
+
+            JsonObject jsonError = Json.createObjectBuilder()
+                    .add("field",violatedField)
+                    .add("violationMessage",message)
+                    .build();
+            jsonArray.add(jsonError);
         }
-
-        public void setCode(int code) {
-            this.code = code;
-        }
-
-        public String getError() {
-            return error;
-        }
-
-        public void setError(String error) {
-            this.error = error;
-        }
+        JsonObject errorJsonEntity = jsonObjectBuilder.add("errors",jsonArray.build()).build();
+        return Response.status(Response.Status.BAD_REQUEST).entity(errorJsonEntity).build();
     }
 }
+
