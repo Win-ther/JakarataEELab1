32c6415aee06a0c8b363b0ccbedb3f8f697efcb8
Daniel Backberg <daniel.backberg@iths.se>
2024-03-02 10:51:57 +0100

    * Added ExceptionMapper class. * Added small test to see if it catches exceptions in HelloResource

--- src/main/java/se/iths/jakartaeelab1/resource/HelloResource.java
+++ src/main/java/se/iths/jakartaeelab1/resource/HelloResource.java
@@ -3,6 +3,8 @@ package se.iths.jakartaeelab1.resource;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello-world")
 public class HelloResource {
@@ -11,4 +13,14 @@ public class HelloResource {
     public String hello() {
         return "Hello, World!";
     }
+
+
+    //This can be removed whenever, just for testing
+    //@GET
+    // @Produces(MediaType.APPLICATION_JSON)
+    //@Path("/test-exception")
+    // public Response testException() {
+    //    throw new RuntimeException("Testing exception");
+    // }
+
 }
