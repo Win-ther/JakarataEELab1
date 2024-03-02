50e53539ca957c13554a56d9d7a5bcbd3d5ba1f4
Daniel Backberg <daniel.backberg@iths.se>
2024-03-02 14:55:19 +0100

    * Delete ExceptionMapper class * Add ConstraintViolationExceptionMapper * Changed ConstraintViolationExceptionMapper to make it avoid IndexOutOfBoundsException
    Currently having issues with testing.

--- src/main/java/se/iths/jakartaeelab1/resource/HelloResource.java
+++ src/main/java/se/iths/jakartaeelab1/resource/HelloResource.java
@@ -3,24 +3,15 @@ package se.iths.jakartaeelab1.resource;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+
 
 @Path("/hello-world")
 public class HelloResource {
+
     @GET
     @Produces("text/plain")
     public String hello() {
         return "Hello, World!";
     }
-
-
-    //This can be removed whenever, just for testing
-    //@GET
-    // @Produces(MediaType.APPLICATION_JSON)
-    //@Path("/test-exception")
-    // public Response testException() {
-    //    throw new RuntimeException("Testing exception");
-    // }
 
 }
